b545775f7948368bff43d66e8ead9fd74479eaf5
sayalipatil26 <sayalipatil26122002@gmail.com>
2022-12-12 19:36:39 +0530

    02_functonAssig.js

--- o2_functonAssig.js
+++ o2_functonAssig.js
@@ -1,11 +1,12 @@
-console.log("======bankDetails 1=============");
-function bankDetails ( ){
-    console.log("HDFC Bank",8956782345,'Pune',631202,"Pune");
 
-    console.log("======bankDetails 2========");
+function bankDetails ( bankName,accountNumber,City,pinCode){
+    console.log("bank details are follows  : ");
+
+    //here some bank details : 
+    console.log( "HDFC Bank",8956782345,'Pune',631202,"Pune");
+
     console.log( bankDetails = "CITI Bank",3456782345,'Pune',431202);
     
-    console.log("======bankDetails 3========");
     console.log("Axis Bank",7856782345,'Mumbai',441202);
 }
 bankDetails();
